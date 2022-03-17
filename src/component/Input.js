87b01e61d0c7aa4cd1baa7dd8c6fdab941760f50
MrYious <rosariomark37@gmail.com>
2022-03-17 18:22:26 +0800

    feat: added UI elements for input component

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,13 +1,38 @@
-import { Grid } from "@mui/material";
+import { Grid, IconButton, TextField } from "@mui/material";
+import { AddBox} from "@mui/icons-material";
 
-export default function Input ({ tasks }){
+export default function Input ({ tasks, addTask }){
+
+    const handleAddTask = () => {
+        console.log(tasks);
+    };
 
     return <>
         <Grid 
             item
             container
-        >
-            {console.log(tasks)}
+            xs={12}
+            spacing={2}
+            justifyContent="center"
+        >   
+            <Grid 
+                item 
+                xs={8}
+            >
+                <TextField
+                    label="Task" 
+                    size="small"
+                    fullWidth
+                />
+            </Grid>
+            <Grid item xs={1}
+            >
+                <IconButton onClick={handleAddTask} >
+                    <AddBox fontSize="inherit"/>
+                </IconButton>
+            </Grid>
+            
+            
         </Grid>
     </>;
 }
--- src/component/ToDo.js
+++ src/component/ToDo.js
@@ -18,6 +18,10 @@ export default function ToDo (){
 
     const [tasks, setTasks] = useState(initTasks);
 
+    const addTask = (task) => {
+        setTasks({...tasks, task});
+    };
+
     return <>
         <Grid 
             container 
@@ -33,14 +37,14 @@ export default function ToDo (){
                 container
                 direction={'column'}
                 justifyContent={'center'}
-                xs={9} md={6}  
+                xs={9} md={6}
                 sx={{
                     //bgcolor: 'gray',                        
                 }}
             >   
                 <Paper elevation={10} sx={{height: '100%', width: '100%', padding: 5,}}>
                     <Header />
-                    <Input tasks={tasks} />
+                    <Input tasks={tasks} addTask={addTask} />
                     {/* Todo Input*/}
                     {/* Todo List*/}
                 </Paper>
